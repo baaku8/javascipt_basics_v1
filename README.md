fe77eeba72b39021cacfdaf8a7ef74d445ce75f0
Piyush Chatterjee <capsicum.broth@gmail.com>
2025-12-27 13:53:26 +0000

    lec5n6_datatypes_and_simple_conversions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascipt_basics_v1
 basic practice and concept building projects of javascript
+
+Javascript official documentation given by ECMA : tc39
+we use "use strict" whenever we use modern form of js, no needed as systems are intelligent now.
---
+++ lec5n6_datatypes_and_conversion.js
@@ -0,0 +1,39 @@
+let stringtype="String";
+let booltype=true;
+let numberType=33;
+let empty;
+
+//common primitive types: 
+//number
+//boolean
+//string
+//null
+//bigInt
+ 
+//typeof is used to know the type of datatype used.
+// console.log(typeof empty); //undefined
+// console.log(typeof null);  //object, yes , type of null is object.
+
+
+//conversion
+//to number
+let val="33";
+let valNum=Number(val);
+// console.log(Number(undefined))
+
+//"33"->33
+//"33a"->NaN
+//true->1, false->0
+//null->0
+//undefined->NaN
+
+//to bool
+// console.log(Boolean(53))
+
+//number>=1 ->true
+//""->false
+//"not empty"->true
+// console.log(Boolean(null)) false;
+// console.log(Boolean(undefined)) false;
+
+//to string->ho hi jayega vai.
